054ed300c7aa68f0e8e57f3e467f2e2708705601
sunfl0w <lucaanthonyschwarz@googlemail.com>
2019-11-04 16:05:13 +0100

    Edited color to be convertable to a string.

--- include/hive/Color.hpp
+++ include/hive/Color.hpp
@@ -1,9 +1,17 @@
 #pragma once
 
+#include <string.h>
+
 namespace Hive {
     enum class Color {
         Red,
         Blue,
         Undefined,
     };
+
+    static const std::string colorStrings[] = {"RED", "BLUE", "UNDEFINED"};
+
+    std::string ColorToString(Hive::Color color) {
+        return colorStrings[(int)color];
+    }
 }
