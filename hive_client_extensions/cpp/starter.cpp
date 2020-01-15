454c1bbe3dbec8cf63e97577b696630d41337d8c
sunfl0w <lucaanthonyschwarz@googlemail.com>
2020-01-15 10:43:50 +0100

    Fixed starter for CPP clients.

--- hive_client_extensions/cpp/starter.cpp
+++ hive_client_extensions/cpp/starter.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[]) {
     AI::HiveLogic logic = AI::HiveLogic();
-    Client::HiveClient client(logic);
-    client.Start(argc, *argv);
+    Client::HiveClient client(&logic);
+    client.Start(argc, argv);
     return 0;
 }
