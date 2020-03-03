9bd5271860c56be96656f7903bf961d8a9971b84
sunfl0w <lucaanthonyschwarz@googlemail.com>
2020-03-03 22:08:55 +0100

    Fixing grasshopper move generation again.

--- src/hive/gameState.cpp
+++ src/hive/gameState.cpp
@@ -235,9 +235,11 @@ namespace Hive {
                 AxialPosition searchPos = grasshopper->GetAxialPosition();
                 searchPos = searchPos.Add(translation);
                 while (searchPos.IsOnBoard()) {
-                    if (!board.PieceStackExists(searchPos) && board.GetPieceStack(searchPos).GetPieceOnTop().GetType() != PieceType::Obstacle && !board.GetNeighbouringPieceStacksExceptObstacles(searchPos).empty()) {
+                    if (!board.PieceStackExists(searchPos) && !board.GetNeighbouringPieceStacksExceptObstacles(searchPos).empty()) {
                         possibleGrasshopperDragMoves.push_back(Move(MoveType::DragMove, currentPlayer.GetColor(), grasshopper->GetAxialPosition(), searchPos, PieceType::Grasshopper));
                         break;
+                    } else if (board.PieceStackExists(searchPos) && board.GetPieceStack(searchPos).GetPieceOnTop().GetType() == PieceType::Obstacle) {
+                        break;  //All moves going further are blocked by obstacle.
                     }
                     searchPos = searchPos.Add(translation);
                 }
